847f1c59971432765d1f36ba9c9790da47512285
gubs <gubs4u@gmail.com>
2017-08-19 18:46:01 -0400

    Add files via upload

---
+++ js/audioAssets.js
@@ -0,0 +1,21 @@
+'use strict';
+
+
+
+// Audio Source - AWS Podcast : https://aws.amazon.com/podcasts/aws-podcast/
+var audioData = [
+	{
+        'title' : 'Gowndamani Comedy',
+        'url' : 'https://s3.amazonaws.com/tamilcomedydialogues/Goundamani+Top+10+One+Liners.mp3'
+    },
+    {
+        'title' : 'Vadivel Winner Movie Comedy',
+        'url' : 'https://s3.amazonaws.com/tamilcomedydialogues/Vadivel-WinnerComedy.mp3'
+    },
+    {
+        'title' : 'Santhanam Boss engira Baskaran Movie Comedy',
+        'url' : 'https://s3.amazonaws.com/tamilcomedydialogues/Santhanam+Arya+Comedy++All+Comedy+Scenes++Boss+Engira+Bhaskaran.mp3'
+    },
+];
+
+module.exports = audioData;
